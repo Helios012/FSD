275ebd504a532fd387f467c628bda42806951ccd
AlphaDev7 <151920580+shivanishetty005@users.noreply.github.com>
2026-04-17 09:28:08 +0530

    Update event_handling.jsx

--- event_handling.jsx
+++ event_handling.jsx
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-class App extends React.Component {
-constructor(props) {
-super(props);
-this.state = {
-companyName: ''
-};
+import React from "react";
+
+function EventExample() {
+
+  function handleClick() {
+    alert("Button clicked");
+  }
+
+  return (
+    <div>
+      <h2>Simple Event Handling Example</h2>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 }
-changeText(event) {
-this.setState({
-companyName: event.target.value
-});
-}
-render() {
-return (
-<div>
-<h2>Simple Event Example</h2>
-<label htmlFor="name">Enter company name: </label>
-<input type="text" id="companyName" onChange={this.changeText.bind(this)}/>
-<h4>You entered: { this.state.companyName }</h4>
-</div>
-);
-}
-}
-export default App;
+
+export default EventExample;
