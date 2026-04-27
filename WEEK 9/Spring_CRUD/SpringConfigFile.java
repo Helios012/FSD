5a78b3c0036e95973ddebefac0523b9d0ca15839
Helios012 <adithyapathakoti@gmail.com>
2026-04-28 01:27:46 +0530

    Create SpringConfigFile.java

---
+++ WEEK 9/Spring_CRUD/SpringConfigFile.java
@@ -0,0 +1,30 @@
+package in.vjit.resources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class SpringConfigFile {
+
+@Bean
+public DriverManagerDataSource myDataSource()
+{
+DriverManagerDataSource dataSource = new DriverManagerDataSource();
+dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+dataSource.setUrl("jdbc:mysql://localhost:3036/spring_jdbc_db");
+dataSource.setUsername("root");
+dataSource.setPassword("root");
+return dataSource;
+}
+
+@Bean
+public JdbcTemplate myJdbcTemplate()
+{
+JdbcTemplate jdbcTemplate = new JdbcTemplate();
+jdbcTemplate.setDataSource(myDataSource());
+return jdbcTemplate;
+}
+
+}
