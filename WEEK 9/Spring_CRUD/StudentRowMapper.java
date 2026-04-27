c09228146ff75755c1af023e87cf992a98bf9cee
Helios012 <adithyapathakoti@gmail.com>
2026-04-28 01:28:43 +0530

    Create StudentRowMapper.java

---
+++ WEEK 9/Spring_CRUD/StudentRowMapper.java
@@ -0,0 +1,27 @@
+package in.vjit.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import in.vjit.beans.Student;
+
+public class StudentRowMapper implements RowMapper<Student>
+{
+
+@Override
+public Student mapRow(ResultSet rs, int rowNum) throws SQLException
+{
+
+Student std = new Student();
+
+std.setRollno(rs.getInt("std_roll"));
+std.setName(rs.getString("std_name"));
+std.setMarks(rs.getFloat("std_marks"));
+
+return std;
+
+}
+
+}
